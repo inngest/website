f23e4fde68d76d289a1c80f1efcaef9f927d4328
Dan Farrelly <daniel.j.farrelly@gmail.com>
2022-02-18 10:30:42 -0500

    Update About page

--- pages/about.js
+++ pages/about.js
@@ -1,35 +1,66 @@
-import { useEffect, useState } from "react";
-import styled from "@emotion/styled";
-import Head from "next/head";
-import Footer from "../shared/footer";
-import Nav from "../shared/nav";
-import Content from "../shared/content";
-import { FinisherHeader } from "../shared/HeaderBG";
+import { useEffect, useState } from "react"
+import styled from "@emotion/styled"
+import Head from "next/head"
+import Footer from "../shared/footer"
+import Nav from "../shared/nav"
+import Content from "../shared/content"
+// import { FinisherHeader } from "../shared/HeaderBG"
 
-import Airplane from "../shared/Icons/Airplane";
-import Audit from "../shared/Icons/Audit";
-import Language from "../shared/Icons/Language";
-import Alert from "../shared/Icons/Alert";
-import Users from "../shared/Icons/Users";
-import VCS from "../shared/Icons/VCS";
+import Block from "../shared/Block"
+
+import Airplane from "../shared/Icons/Airplane"
+import Audit from "../shared/Icons/Audit"
+import Language from "../shared/Icons/Language"
+import Alert from "../shared/Icons/Alert"
+import Users from "../shared/Icons/Users"
+import VCS from "../shared/Icons/VCS"
+
+const MISSION = "Accelerate the adoption of event based architecture."
+const VISION = "World class architecture accessible to everyone."
+
+const TEAM = [
+  {
+    name: "Tony Holdstock-Brown",
+    role: "CEO & Founder",
+    bio: "Former Head of Engineering at Uniform Teeth",
+    avatar: "/team/tony-2022-02-18.jpg",
+  },
+  {
+    name: "Dan Ramich",
+    role: "Founding Engineer",
+    bio: "Former Staff Engineer at Rancher (SUSE)",
+    avatar: "/team/dan-r-2022-02-18.jpg",
+  },
+  {
+    name: "Dan Farrelly",
+    role: "Foudning Engineer",
+    bio: "Former CTO at Buffer",
+    avatar: "/team/dan-f-2022-02-18.jpg",
+  },
+]
+
+const INVESTORS = [
+  {
+    name: "Afore.vc",
+  },
+  {
+    name: "Kleiner Perkins",
+  },
+  {
+    name: "Banana Capital",
+  },
+]
 
 export default function Home() {
-  useEffect(() => {
-    gradient(document.querySelector(".hero"));
-  }, []);
-
   return (
-    <>
+    <Page>
       <Head>
         <title>Inngest: About Us</title>
         <link rel="icon" href="/favicon.png" />
         <meta property="og:title" content="Inngest" />
         <meta property="og:url" content="https://www.inngest.com" />
         <meta property="og:image" content="/logo.svg" />
-        <meta
-          property="og:description"
-          content="All about the company that started the event-driven serverless movement"
-        />
+        <meta property="og:description" content={MISSION} />
         <script src="/inngest-sdk.js"></script>
         <script
           defer
@@ -41,81 +72,84 @@ export default function Home() {
       <Nav />
 
       <Hero className="hero">
-        <Content className="grid">
-          <div>
-            <h1>About</h1>
-            <p>
-              We're creating a new way of deploying serverless functions for
-              developers that's faster, more reliable, and easier to
-              grow&nbsp;and&nbsp;scale.
-            </p>
-          </div>
+        <Content>
+          <Label>Our Mission</Label>
+          <h1>{MISSION}</h1>
+          <p>
+            We're creating a new way of deploying serverless functions for
+            developers that's faster, more reliable, and easier to
+            grow&nbsp;and&nbsp;scale.
+          </p>
         </Content>
       </Hero>
 
-      <Content></Content>
+      <Content>
+        <h2>Team</h2>
+        <Grid>
+          {TEAM.map((person) => {
+            return (
+              <Block>
+                <Avatar src={person.avatar} />
+                <h3>{person.name}</h3>
+                <p>
+                  <strong>{person.role}</strong> - {person.bio}
+                </p>
+              </Block>
+            )
+          })}
+        </Grid>
+
+        <h2>Investors</h2>
+        <Grid>
+          {INVESTORS.map((person) => {
+            return (
+              <Block>
+                <h3>{person.name}</h3>
+              </Block>
+            )
+          })}
+        </Grid>
+      </Content>
 
       <div style={{ marginTop: 100 }}>
         <Footer />
       </div>
-    </>
-  );
+    </Page>
+  )
 }
 
-const gradient = (el, colors = ["#18435c", "#18435c", "#2f622f"]) => {
-  new FinisherHeader(
-    {
-      count: 6,
-      size: {
-        min: 1000,
-        max: 1800,
-        pulse: 0,
-      },
-      speed: {
-        x: {
-          min: 0.1,
-          max: 1.1,
-        },
-        y: {
-          min: 0.1,
-          max: 1,
-        },
-      },
-      colors: {
-        background: "#0f111e",
-        particles: colors,
-      },
-      blending: "lighten",
-      opacity: {
-        center: 0.5,
-        edge: 0,
-      },
-      shapes: ["c"],
-    },
-    el
-  );
-};
+const Page = styled.div`
+  background: url(/assets/hero-grid.svg) no-repeat right top;
+`
+
+const Avatar = styled.img`
+  border-radius: 50%;
+  width: 5rem;
+  height: 5rem;
+  margin-bottom: 1rem;
+`
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 2rem 2rem;
+  margin: 2rem 0;
+`
+
+const Label = styled.p`
+  font-size: 0.7rem;
+  text-transform: uppercase;
+  margin: 0.5rem 0;
+  font-family: var(--font-mono);
+`
 
 const Hero = styled.div`
-  position: relative;
-  z-index: 2;
-  overflow: hidden;
+  margin: 4rem 0;
 
-  .grid {
-    position: relative;
-    z-index: 2;
-    text-align: center;
-
-    > div:first-of-type {
-      z-index: 2;
-      padding: 13vh 0;
-      max-width: 700px;
-      margin: 0 auto;
-    }
+  h1 {
+    font-size: 2rem;
   }
-
-  h1 + p {
-    font-size: 22px;
-    line-height: 1.45;
+  p {
+    max-width: 36rem;
   }
-`;
+`
