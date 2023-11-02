64e9a4575a33b282547541cca5e2f232d0551bad
Dan Farrelly <daniel.j.farrelly@gmail.com>
2023-11-02 17:15:51 -0400

    Fix nested section display in docs nav
    router.asPath is different on the client and the server it seems during testing where the client side use includes a hash or search params. This removes those alwayas to normalize.

--- shared/Docs/Navigation.tsx
+++ shared/Docs/Navigation.tsx
@@ -243,10 +243,12 @@ function findRecursiveSectionLinkMatch(nav, pathname) {
     });
   });
 }
+// todo fix active on top level
 
 export function Navigation(props) {
   const router = useRouter();
-  const pathname = router.asPath;
+  // Remove query params and hash from pathname
+  const pathname = router.asPath.replace(/(\?|#).+$/, "");
 
   const nestedSection = findRecursiveSectionLinkMatch(topLevelNav, pathname);
   const isNested = !!nestedSection;
@@ -292,10 +294,9 @@ export function Navigation(props) {
                 </h2>
                 <ul role="list" className="mt-3 flex flex-col gap-2">
                   {item.links.map((link, idx) => (
-                    <li>
+                    <li key={idx}>
                       <NavLink
                         href={link.href}
-                        key={idx}
                         isTopLevel={true}
                         tag={link.tag}
                       >
