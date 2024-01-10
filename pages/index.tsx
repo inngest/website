d9d66afb18ae3be3071c848143404e93c73639f5
Dan Farrelly <daniel.j.farrelly@gmail.com>
2024-01-10 16:02:49 -0500

    Update banner design, move to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ import Header from "../shared/Header";
 import Hero from "../shared/Home/Hero";
 import Logos from "src/shared/Home/Logos";
 
-// import SDKOverview from "src/shared/Home/SDKOverview";
 import UseCases from "src/shared/Home/UseCases";
 import Features from "src/shared/Home/Features";
 import LocalDev from "../shared/Home/LocalDev";
@@ -15,14 +14,11 @@ import Footer from "../shared/Footer";
 import Quote from "src/shared/Home/Quote";
 import SocialProof from "src/shared/Home/SocialProof";
 
-// import GetThingsShipped from "src/shared/Home/GetThingsShipped";
-// import RunAnywhere from "src/shared/Home/RunAnywhere";
-// import PlatformFeatures from "src/shared/Home/PlatformFeatures";
 import PlatformFeatures2 from "src/shared/Home/PlatformFeatures2";
-// import FeatureCallouts from "src/shared/Home/FeatureCallouts";
 import Flexibility from "src/shared/Home/Flexibility";
 import type { PageProps } from "src/shared/types";
-// import EnterpriseTrust from "src/shared/Home/EnterpriseTrust";
+import PageBanner from "../shared/legacy/PageBanner";
+import { RocketLaunchIcon } from "@heroicons/react/24/outline";
 
 export async function getStaticProps(): Promise<
   GetStaticPropsResult<PageProps>
@@ -51,6 +47,15 @@ export default function Home() {
       }}
     >
       <Header />
+
+      <PageBanner href="/launch-week?ref=homepage-banner">
+        <RocketLaunchIcon className="inline-flex h-5 mr-1" />
+        <span className="sm:hidden">Join Us for Launch Week! Jan 22nd</span>
+        <span className="hidden sm:inline">
+          Join Us for Launch Week!{" "}
+          <span className="font-normal">Starts January 22nd</span>
+        </span>
+      </PageBanner>
 
       <Hero />
 
--- shared/legacy/PageBanner.tsx
+++ shared/legacy/PageBanner.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
 
 type Props = {
   href: string;
@@ -8,10 +9,13 @@ type Props = {
 const PageBanner: React.FC<Props> = ({ href, children }) => (
   <a
     href={href}
-    className="page-banner bg-indigo-500 text-sm block text-center w-full py-2 text-white font-medium hover:bg-indigo-600 transition-all"
+    className="page-banner flex items-center justify-center gap-2.5 w-full py-2 tracking-tight bg-[#A7F3D0] bg-gradient-to-r from-[#5EEAD4] via-[#A7F3D0] to-[#FDE68A] hover:from-[#5EEAD4] hover:via-[#C9EEB5] hover:to-[#FDE68A] text-base text-slate-900 font-bold transition-all"
   >
     {children}
-    <span className="text-white"> &rsaquo;</span>
+    <span className="inline-flex items-center ml-2 text-slate-900">
+      {" "}
+      Learn more <ChevronRightIcon className="h-5" />
+    </span>
   </a>
 );
 
