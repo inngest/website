5758e7bfb0073e24c8d64996f01b8d0001f45fbe
Dan Farrelly <daniel.j.farrelly@gmail.com>
2024-01-12 11:35:50 -0500

    Refresh nav links to feature case studies and update docs

--- pages/index.tsx
+++ pages/index.tsx
@@ -92,7 +92,7 @@ export default function Home() {
               href="/customers"
               className="mx-auto rounded-md font-medium px-6 py-2 bg-slate-800 hover:bg-slate-600 transition-all text-white border border-slate-800 hover:border-slate-600 hover:bg-slate-500/10 whitespace-nowrap"
             >
-              Read customer success stories
+              Read customer case studies
             </Link>
           </div>
         }
