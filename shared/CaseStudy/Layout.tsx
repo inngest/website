eca5b6c03df827c88fa4b74c7cf32ea8bdbe0076
Dan Farrelly <daniel.j.farrelly@gmail.com>
2024-01-12 09:07:49 -0500

    INN-2593: Add Aomni case study

--- shared/CaseStudy/Layout.tsx
+++ shared/CaseStudy/Layout.tsx
@@ -74,31 +74,33 @@ export function Layout({
                 {title}
               </h1>
 
-              <blockquote className="mx-auto my-8 max-w-3xl px-8 md:p-16 flex flex-col md:flex-row gap-8 bg-[url(/assets/textures/wave.svg)] bg-[length:auto_80%] bg-center bg-no-repeat">
-                <p className="text-lg leading-7 relative">
-                  <span className="absolute top-1 -left-4 text-2xl leading-3 text-slate-400/80">
-                    &ldquo;
-                  </span>
-                  {quote.text}
-                  <span className="ml-1 text-2xl leading-3 text-slate-400/80">
-                    &rdquo;
-                  </span>
-                </p>
-                <footer className="min-w-[180px] flex flex-col gap-4">
-                  <Image
-                    src={quote.avatar}
-                    alt={`Image of ${quote.attribution.name}`}
-                    height="72"
-                    width="72"
-                    className="rounded-full h-12 w-12 lg:h-20 lg:w-20"
-                  />
-                  <cite className="text-slate-300 leading-8 not-italic">
-                    <span className="text-lg">{quote.attribution.name}</span>
-                    <br />
-                    <span className="text-sm">{quote.attribution.title}</span>
-                  </cite>
-                </footer>
-              </blockquote>
+              {quote && (
+                <blockquote className="mx-auto my-8 max-w-3xl px-8 md:p-16 flex flex-col md:flex-row gap-8 bg-[url(/assets/textures/wave.svg)] bg-[length:auto_80%] bg-center bg-no-repeat">
+                  <p className="text-lg leading-7 relative">
+                    <span className="absolute top-1 -left-4 text-2xl leading-3 text-slate-400/80">
+                      &ldquo;
+                    </span>
+                    {quote.text}
+                    <span className="ml-1 text-2xl leading-3 text-slate-400/80">
+                      &rdquo;
+                    </span>
+                  </p>
+                  <footer className="min-w-[180px] flex flex-col gap-4">
+                    <Image
+                      src={quote.avatar}
+                      alt={`Image of ${quote.attribution.name}`}
+                      height="72"
+                      width="72"
+                      className="rounded-full h-12 w-12 lg:h-20 lg:w-20"
+                    />
+                    <cite className="text-slate-300 leading-8 not-italic">
+                      <span className="text-lg">{quote.attribution.name}</span>
+                      <br />
+                      <span className="text-sm">{quote.attribution.title}</span>
+                    </cite>
+                  </footer>
+                </blockquote>
+              )}
 
               <div className="md:max-w-[70ch] prose mt-12 mb-20 prose-img:rounded-lg prose-code:bg-slate-800 prose-code:tracking-tight text-slate-300 prose-a:text-indigo-400 prose-a:no-underline hover:prose-a:underline hover:prose-a:text-white prose-a:font-medium prose-a:transition-all prose-invert blog-content">
                 {children}
