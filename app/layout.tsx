e0a922304910c8c0becd54bbb0a6972a58692e78
Dan Farrelly <daniel.j.farrelly@gmail.com>
2024-04-15 14:08:18 -0400

    Add Inngest SDK page views

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import Header from "src/shared/Header";
 import Footer from "src/shared/Footer";
 import Script from "next/script";
-import { useAnonymousID } from "../shared/trackingHooks";
+import { HeaderInit, PageViews } from "../components/InngestClientSDK";
 
 import "./globals.css";
 
@@ -20,19 +20,7 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css"
         />
-        <script
-          // We use a simple array queue to send any events after the SDK is loaded
-          // These are sent onLoad where the script is loaded in _app.js
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-                window._inngestQueue = [];
-                if (typeof window.Inngest === "undefined") {
-                  window.Inngest = { event: function(p){ window._inngestQueue.push(p); } };
-                }
-              `,
-          }}
-        />
+        <HeaderInit />
       </head>
       <body className="home font-sans bg-slate-1000">
         <Header />
@@ -43,39 +31,7 @@ export default function RootLayout({
 
         <Footer disableCta={true} />
 
-        {/* <Script
-          id="js-inngest-sdk-script"
-          strategy="afterInteractive"
-          src="/inngest-sdk.js"
-          onLoad={() => {
-            window.Inngest.init(process.env.NEXT_PUBLIC_INNGEST_KEY);
-            window.Inngest.identify({ anonymous_id: anonymousID });
-            // The hook should tell us if the anon id is an existing one, or it's just been set
-            const firstTouch = !existing;
-            let ref = null;
-            try {
-              const urlParams = new URLSearchParams(window.location.search);
-              ref = urlParams.get("ref");
-            } catch (e) {}
-            // See tracking for next/link based transitions in tracking.ts
-            window.Inngest.event({
-              name: "website/page.viewed",
-              data: {
-                first_touch: firstTouch,
-                ref: ref,
-              },
-              v: "2022-12-27.1",
-            });
-            if (typeof window !== "undefined" && window._inngestQueue.length) {
-              window._inngestQueue.forEach((p) => {
-                // Prevent the double tracking of page views b/c routeChangeComplete
-                // is unpredictable.
-                if (p.name === "website/page.viewed") return;
-                window.Inngest.event(p);
-              });
-            }
-          }}
-        /> */}
+        <PageViews />
         <script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG_ID}`}
