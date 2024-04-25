3af1652417863d1555b5be38ad3675e117a96bb1
Dan Farrelly <daniel.j.farrelly@gmail.com>
2024-04-25 14:23:22 -0400

    Add fetch function run status and output

--- shared/Docs/navigationStructure.ts
+++ shared/Docs/navigationStructure.ts
@@ -577,6 +577,10 @@ const sectionExamples: NavGroup[] = [
         title: "Email Sequence",
         href: `/docs/examples/email-sequence`,
       },
+      {
+        title: "Fetch run status and output",
+        href: `/docs/examples/fetch-run-status-and-output`,
+      },
     ],
   },
 ];
