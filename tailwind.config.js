3c7fcfdef614d11728ba18ab5b9e1f43928e63d7
Sylwia Vargas <sylwia.vargas@gmail.com>
2024-05-17 14:06:12 +0100

    adds mdx to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,10 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   darkMode: "class",
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./shared/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./shared/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     typography: require("./typography"),
