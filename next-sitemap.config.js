987f003b72d954753ec5d645e522597d1ad0eff3
Dan Farrelly <daniel.j.farrelly@gmail.com>
2024-06-12 15:13:28 -0400

    Prevent landing pages from indexing

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   siteUrl: "https://www.inngest.com",
   generateRobotsTxt: true,
-  // Don't index the _underscore prefix directories
-  exclude: ["*/_*"],
+  exclude: [
+    // Don't index the _underscore prefix directories
+    "*/_*",
+    // Skip landing page from indexing
+    "*/landing/*",
+  ],
 };
