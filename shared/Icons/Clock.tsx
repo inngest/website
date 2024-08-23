442c334456b178a9189907910dec348102097737
Charly POLY <charly@inngest.com>
2024-08-23 15:59:25 +0200

    fix(docs): many icon color fixes

--- shared/Icons/Clock.tsx
+++ shared/Icons/Clock.tsx
@@ -3,8 +3,8 @@ import * as React from "react";
 function SvgComponent(props) {
   return (
     <svg
-      width={20}
-      height={20}
+      width={18}
+      height={18}
       viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
