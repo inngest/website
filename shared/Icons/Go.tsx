00a10ffc28c670d06a6dee13440c3a4b7de191dc
Ana <anafilipadealmeida@gmail.com>
2024-08-29 12:41:55 +0100

    Replace icons

--- shared/Icons/Go.tsx
+++ shared/Icons/Go.tsx
@@ -1,32 +1,157 @@
-import React from "react";
-import type { IconProps } from "./props";
-import clsx from "clsx";
-
-const Go = ({ size = 8, fill = "primary", className }: IconProps) => {
-  const twClass =
-    fill && fill === "black"
-      ? `fill-${fill} dark:fill-white`
-      : `fill-${fill ? fill : "primary"}`;
+export default function IconGo({ className, size = 18 }: { className?: string; size?: number }) {
   return (
-    <svg
-      viewBox="0 0 72 72"
-      className={clsx(`h-${size} w-${size}`, className)}
-      height={size}
-      width={size}
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <svg className={className} height={size} viewBox="0 0 128 128" width={size}>
+      <defs>
+        <path id="go-original-a" d="M18.8 1h90.5v126H18.8z"></path>
+      </defs>
+
       <path
-        d="M35.2803 27.9449C32.3572 28.6871 30.3621 29.2439 27.4855 29.9863C26.7896 30.1718 26.7433 30.2182 26.14 29.5223C25.4441 28.7335 24.9337 28.2232 23.9594 27.7592C21.0365 26.321 18.2063 26.7385 15.5616 28.4551C12.4066 30.4966 10.7827 33.5125 10.8291 37.2705C10.8756 40.9823 13.4274 44.0444 17.0928 44.5548C20.2477 44.9724 22.8922 43.8589 24.9802 41.4927C25.3977 40.9823 25.769 40.4256 26.2329 39.776C24.5626 39.776 22.4747 39.776 17.2783 39.776C16.304 39.776 16.072 39.1728 16.3968 38.3841C16.9999 36.9458 18.1134 34.5331 18.763 33.3268C18.9022 33.0484 19.2269 32.5845 19.9228 32.5845C22.2891 32.5845 31.0117 32.5845 36.8113 32.5845C36.7185 33.8372 36.7185 35.0899 36.5329 36.3427C36.0225 39.6832 34.7699 42.7454 32.7284 45.4364C29.3878 49.8441 25.0265 52.5814 19.5053 53.3239C14.9585 53.927 10.7364 53.0455 7.02456 50.2616C3.59125 47.6634 1.64264 44.2301 1.13224 39.9615C0.529148 34.9043 2.01385 30.3574 5.07595 26.3673C8.37008 22.0524 12.7313 19.315 18.0671 18.3407C22.4283 17.5519 26.6041 18.0623 30.3621 20.6142C32.8213 22.2381 34.5843 24.4651 35.7442 27.1561C36.0225 27.5737 35.837 27.8057 35.2803 27.9449Z"
-        className={twClass}
-        fill-rule="evenodd"
-      />
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M21.1 68.7c.2 3.5 3.7 1.9 5.3.8 1.5-1.1 2-.2 2.1-2.3.1-1.4.2-2.7.2-4.1-2.3-.2-4.8.3-6.7 1.7-.9.7-2.8 3-.9 3.9"
+        fillRule="evenodd"
+        fill="currentColor"
+      ></path>
+
       <path
-        d="M50.6376 53.6021C46.4156 53.5092 42.5645 52.3031 39.3168 49.5192C36.5794 47.153 34.8627 44.1371 34.3059 40.5646C33.4708 35.3217 34.9092 30.6821 38.0641 26.5528C41.4511 22.0987 45.534 19.7789 51.0552 18.8046C55.7877 17.9695 60.2416 18.4334 64.2782 21.1708C67.9435 23.6763 70.217 27.0632 70.8201 31.5172C71.6089 37.7809 69.7995 42.8844 65.4845 47.2457C62.4223 50.3543 58.6643 52.3031 54.3493 53.1845C53.0967 53.4165 51.844 53.4629 50.6376 53.6021ZM61.68 34.8578C61.6337 34.2547 61.6337 33.7907 61.5408 33.3268C60.7057 28.7335 56.4836 26.1353 52.0759 27.1559C47.7609 28.1302 44.9772 30.8678 43.9564 35.229C43.1213 38.8479 44.8844 42.5132 48.225 43.9979C50.7768 45.1116 53.3285 44.9724 55.7877 43.7195C59.453 41.8173 61.448 38.8479 61.68 34.8578Z"
-        className={twClass}
-        fill-rule="evenodd"
-      />
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M21.1 68.7c.5-.2 1.1-.3 1.4-.8"
+        fillRule="evenodd"
+        fill="currentColor"
+      ></path>
+
+      <path
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M29.3 26.4c-13.6-3.8-3.5-21.1 7.4-14l-7.4 14z"
+        fillRule="evenodd"
+        fill="currentColor"
+      ></path>
+
+      <path
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M89.6 11.1c10.7-7.5 20.5 9.5 8 13.8l-8-13.8z"
+        fillRule="evenodd"
+        fill="currentColor"
+      ></path>
+      <path
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M92 112.3c2.7 1.7 7.7 6.8 3.6 9.3-3.9 3.6-6.1-4-9.6-5 1.5-2 3.4-3.9 6-4.3"
+        fillRule="evenodd"
+        fill="currentColor"
+      ></path>
+
+      <path
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M43.2 118.1c-3.2.5-5 3.4-7.7 4.9-2.5 1.5-3.5-.5-3.7-.9-.4-.2-.4.2-1-.4-2.3-3.7 2.4-6.4 4.9-8.2 3.5-.8 5.7 2.2 7.5 4.6"
+        fillRule="evenodd"
+        fill="currentColor"
+      ></path>
+
+      <path
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M107.1 68.2c-.2 3.5-3.7 1.9-5.3.8-1.5-1.1-2-.2-2.1-2.3-.1-1.4-.2-2.7-.2-4.1 2.3-.2 4.8.3 6.7 1.7 1 .8 2.8 3 .9 3.9"
+        fillRule="evenodd"
+        fill="currentColor"
+      ></path>
+
+      <path
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M107.1 68.2c-.5-.2-1.1-.3-1.4-.8"
+        fillRule="evenodd"
+        fill="currentColor"
+      ></path>
+
+      <path
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M62.8 4c13.6 0 26.3 1.9 33 15 6 14.6 3.8 30.4 4.8 45.9.8 13.3 2.5 28.6-3.6 40.9-6.5 12.9-22.7 16.2-36 15.7-10.5-.4-23.1-3.8-29.1-13.4-6.9-11.2-3.7-27.9-3.2-40.4.6-14.8-4-29.7.9-44.1C34.5 8.5 48.1 5.1 62.8 4"
+        fillRule="evenodd"
+        fill="currentColor"
+      ></path>
+
+      <path
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M65.2 22.2c2.4 14.2 25.6 10.4 22.3-3.9-3-12.8-23.1-9.2-22.3 3.9"
+        fillRule="evenodd"
+        fill="#fff"
+      ></path>
+
+      <path
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M37.5 24.5c3.2 12.3 22.9 9.2 22.2-3.2-.9-14.8-25.3-12-22.2 3.2"
+        fillRule="evenodd"
+        fill="#fff"
+      ></path>
+
+      <path
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M68 39.2c0 1.8.4 3.9.1 5.9-.5.9-1.4 1-2.2 1.3-1.1-.2-2-.9-2.5-1.9-.3-2.2.1-4.4.2-6.6l4.4 1.3z"
+        fillRule="evenodd"
+        fill="#fff"
+      ></path>
+
+      <path
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M46.3 22.5c0 2-1.5 3.6-3.3 3.6-1.8 0-3.3-1.6-3.3-3.6s1.5-3.6 3.3-3.6c1.8 0 3.3 1.6 3.3 3.6"
+        fillRule="evenodd"
+        fill="currentColor"
+      ></path>
+      <path
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M45.2 23.3c0 .5-.4.9-.8.9s-.8-.4-.8-.9.4-.9.8-.9c.5 0 .8.4.8.9"
+        fillRule="evenodd"
+        fill="#fff"
+      ></path>
+      <path
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M74.2 21.6c0 2-1.5 3.6-3.3 3.6-1.8 0-3.3-1.6-3.3-3.6s1.5-3.6 3.3-3.6c1.8 0 3.3 1.6 3.3 3.6"
+        fillRule="evenodd"
+        fill="currentColor"
+      ></path>
+      <path
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M73.2 22.4c0 .5-.3.9-.8.9-.4 0-.8-.4-.8-.9s.3-.9.8-.9c.4 0 .8.4.8.9M58.4 39c-1.5 3.5.8 10.6 4.8 5.4-.3-2.2.1-4.4.2-6.6l-5 1.2z"
+        fillRule="evenodd"
+        fill="#fff"
+      ></path>
+      <path
+        clipPath="url(#go-original-b)"
+        d="M60.5 46.6c-.7 0-1.4-.4-1.9-1.2-1.1-1.6-1.3-4.6-.5-6.5l.1-.2 5.5-1.4v.4l-.1 2.2c-.1 1.5-.2 2.9 0 4.4v.1l-.1.1c-1 1.4-2 2.1-3 2.1zm-1.8-7.3c-.6 1.7-.4 4.4.5 5.7.4.6.8.9 1.3.9.7 0 1.5-.6 2.3-1.6-.2-1.5-.1-3 .1-4.4l.1-1.7-4.3 1.1z"
+        fill="currentColor"
+      ></path>
+      <path
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M58.9 32.2c-2.7.2-4.9 3.5-3.5 6 1.9 3.4 6-.3 8.6 0 3 .1 5.4 3.2 7.8.6 2.7-2.9-1.2-5.7-4.1-7l-8.8.4z"
+        fillRule="evenodd"
+        fill="white"
+      ></path>
+      <path
+        clipPath="url(#go-original-b)"
+        d="M69.7 40.2c-.9 0-1.8-.4-2.7-.8-.9-.4-1.9-.8-3-.8h-.3c-.8 0-1.7.3-2.7.7-1.1.4-2.2.7-3.2.7-1.2 0-2.1-.5-2.7-1.6-.7-1.2-.6-2.6.1-3.9.8-1.5 2.2-2.4 3.7-2.6l8.9-.4h.1c2.2.9 4.7 2.6 5.2 4.6.2 1-.1 2-.9 2.9-.8.9-1.6 1.2-2.5 1.2zM64.1 38c1.1 0 2.2.5 3.2.9.9.4 1.7.7 2.5.7.7 0 1.3-.3 1.9-.9.7-.7.9-1.5.8-2.3-.4-1.7-2.8-3.3-4.7-4.1l-8.7.4c-1.3.1-2.5 1-3.2 2.2-.6 1.1-.6 2.3-.1 3.3.5.9 1.1 1.3 2.1 1.3.9 0 1.9-.4 2.9-.7 1.1-.4 2-.7 3-.7 0-.2.1-.2.3-.1z"
+        fill="currentColor"
+      ></path>
+      <path
+        clipPath="url(#go-original-b)"
+        clipRule="evenodd"
+        d="M58.6 32.1c-.2-4.7 8.8-5.3 9.8-1.4 1.1 4-9.4 4.9-9.8 1.4"
+        fillRule="evenodd"
+        fill="white"
+      ></path>
     </svg>
   );
-};
-
-export default Go;
+}
