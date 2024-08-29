f983bb97dc3f2f592c4d6696686e02f8350b8f86
Ana <anafilipadealmeida@gmail.com>
2024-08-29 10:41:15 +0100

    Add tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,71 +13,257 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./shared/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  // navigation dynamic nested groups support
-  safelist: [
-    "ml-4",
-    "ml-8",
-    "stroke-black",
-    "fill-black",
-    "stroke-primary",
-    "sm:grid-cols-3",
-  ],
   theme: {
     typography: require("./typography"),
     extend: {
       colors: {
-        // Aliases for ease of re-design
-        body: colors.slate[100],
-        // compat of different primary color for website and docs
-        primary: "var(--primary-color)",
-        inngestBlack: theme2024.black,
-        // Design System 2024
-        carbon: {
-          0: "#fefefe",
-          50: "#f6f6f6",
-          100: "#e2e2e2",
-          200: "#cccccc",
-          300: "#b0b0b0",
-          400: "#9b9b9b",
-          500: "#7c7c7c",
-          600: "#636363",
-          700: "#4b4b4b",
-          800: "#353535",
-          900: "#242424",
-          950: "#121212",
-          1000: "#020202",
-        },
-        matcha: {
-          0: "#eff9f2",
-          100: "#dff5e6",
-          200: "#c4efd4",
-          300: "#9adab3",
-          400: "#66bd8b",
-          500: "#2c9b63",
-          600: "#027a48",
-          700: "#016239",
-          800: "#015430",
-          900: "#004d2b",
-        },
-        breeze: {
-          0: "#eff7ff",
-          100: "#e0f2ff",
-          200: "#c4efd4",
-          300: "#9cd2ff",
-          400: "#52b2fd",
-          500: "#2389f1",
-          600: "#1365d6",
-          700: "#1450b1",
-          800: "#17439b",
-          900: "#134085",
-          1000: "#00050B",
-        },
-        // Extend base tailwind colors
+        // Legacy extensions
         slate: {
           950: "#0C1323",
           1000: "#080D19",
           1050: "#050911",
         },
+        // Aliases for ease of re-design
+        body: colors.slate[100],
+        primary: colors.indigo["500"],
+        inngestBlack: theme2024.black,
+        background: theme2024.black, // TODO - Change to carbon-1000
+
+        // Design System 2024
+        carbon: {
+          0: "rgb(var(--color-carbon-0) / <alpha-value>)",
+          50: "rgb(var(--color-carbon-50) / <alpha-value>)",
+          100: "rgb(var(--color-carbon-100) / <alpha-value>)",
+          200: "rgb(var(--color-carbon-200) / <alpha-value>)",
+          300: "rgb(var(--color-carbon-300) / <alpha-value>)",
+          400: "rgb(var(--color-carbon-400) / <alpha-value>)",
+          500: "rgb(var(--color-carbon-500) / <alpha-value>)",
+          600: "rgb(var(--color-carbon-600) / <alpha-value>)",
+          700: "rgb(var(--color-carbon-700) / <alpha-value>)",
+          800: "rgb(var(--color-carbon-800) / <alpha-value>)",
+          900: "rgb(var(--color-carbon-900) / <alpha-value>)",
+          1000: "rgb(var(--color-carbon-1000) / <alpha-value>)",
+        },
+        matcha: {
+          0: "rgb(var(--color-matcha-0) / <alpha-value>)",
+          100: "rgb(var(--color-matcha-100) / <alpha-value>)",
+          200: "rgb(var(--color-matcha-200) / <alpha-value>)",
+          300: "rgb(var(--color-matcha-300) / <alpha-value>)",
+          400: "rgb(var(--color-matcha-400) / <alpha-value>)",
+          500: "rgb(var(--color-matcha-500) / <alpha-value>)",
+          600: "rgb(var(--color-matcha-600) / <alpha-value>)",
+          700: "rgb(var(--color-matcha-700) / <alpha-value>)",
+          800: "rgb(var(--color-matcha-800) / <alpha-value>)",
+          900: "rgb(var(--color-matcha-900) / <alpha-value>)",
+        },
+        breeze: {
+          0: "rgb(var(--color-breeze-0) / <alpha-value>)",
+          100: "rgb(var(--color-breeze-100) / <alpha-value>)",
+          200: "rgb(var(--color-breeze-200) / <alpha-value>)",
+          300: "rgb(var(--color-breeze-300) / <alpha-value>)",
+          400: "rgb(var(--color-breeze-400) / <alpha-value>)",
+          500: "rgb(var(--color-breeze-500) / <alpha-value>)",
+          600: "rgb(var(--color-breeze-600) / <alpha-value>)",
+          700: "rgb(var(--color-breeze-700) / <alpha-value>)",
+          800: "rgb(var(--color-breeze-800) / <alpha-value>)",
+          900: "rgb(var(--color-breeze-900) / <alpha-value>)",
+        },
+        ruby: {
+          0: "rgb(var(--color-ruby-0) / <alpha-value>)",
+          100: "rgb(var(--color-ruby-100) / <alpha-value>)",
+          200: "rgb(var(--color-ruby-200) / <alpha-value>)",
+          300: "rgb(var(--color-ruby-300) / <alpha-value>)",
+          400: "rgb(var(--color-ruby-400) / <alpha-value>)",
+          500: "rgb(var(--color-ruby-500) / <alpha-value>)",
+          600: "rgb(var(--color-ruby-600) / <alpha-value>)",
+          700: "rgb(var(--color-ruby-700) / <alpha-value>)",
+          800: "rgb(var(--color-ruby-800) / <alpha-value>)",
+          900: "rgb(var(--color-ruby-900) / <alpha-value>)",
+        },
+        honey: {
+          0: "rgb(var(--color-honey-0) / <alpha-value>)",
+          100: "rgb(var(--color-honey-100) / <alpha-value>)",
+          200: "rgb(var(--color-honey-200) / <alpha-value>)",
+          300: "rgb(var(--color-honey-300) / <alpha-value>)",
+          400: "rgb(var(--color-honey-400) / <alpha-value>)",
+          500: "rgb(var(--color-honey-500) / <alpha-value>)",
+          600: "rgb(var(--color-honey-600) / <alpha-value>)",
+          700: "rgb(var(--color-honey-700) / <alpha-value>)",
+          800: "rgb(var(--color-honey-800) / <alpha-value>)",
+          900: "rgb(var(--color-honey-900) / <alpha-value>)",
+        },
+        blush: {
+          0: "rgb(var(--color-blush-0) / <alpha-value>)",
+          100: "rgb(var(--color-blush-100) / <alpha-value>)",
+          200: "rgb(var(--color-blush-200) / <alpha-value>)",
+          300: "rgb(var(--color-blush-300) / <alpha-value>)",
+          400: "rgb(var(--color-blush-400) / <alpha-value>)",
+          500: "rgb(var(--color-blush-500) / <alpha-value>)",
+          600: "rgb(var(--color-blush-600) / <alpha-value>)",
+          700: "rgb(var(--color-blush-700) / <alpha-value>)",
+          800: "rgb(var(--color-blush-800) / <alpha-value>)",
+          900: "rgb(var(--color-blush-900) / <alpha-value>)",
+        },
+        purplehaze: {
+          0: "rgb(var(--color-purplehaze-0) / <alpha-value>)",
+          100: "rgb(var(--color-purplehaze-100) / <alpha-value>)",
+          200: "rgb(var(--color-purplehaze-200) / <alpha-value>)",
+          300: "rgb(var(--color-purplehaze-300) / <alpha-value>)",
+          400: "rgb(var(--color-purplehaze-400) / <alpha-value>)",
+          500: "rgb(var(--color-purplehaze-500) / <alpha-value>)",
+          600: "rgb(var(--color-purplehaze-600) / <alpha-value>)",
+          700: "rgb(var(--color-purplehaze-700) / <alpha-value>)",
+          800: "rgb(var(--color-purplehaze-800) / <alpha-value>)",
+          900: "rgb(var(--color-purplehaze-900) / <alpha-value>)",
+        },
+        primary: {
+          "2xSubtle": "rgb(var(--color-primary-2xSubtle) / <alpha-value>)",
+          xSubtle: "rgb(var(--color-primary-xSubtle) / <alpha-value>)",
+          subtle: "rgb(var(--color-primary-subtle) / <alpha-value>)",
+          moderate: "rgb(var(--color-primary-moderate) / <alpha-value>)",
+          intense: "rgb(var(--color-primary-intense) / <alpha-value>)",
+          xIntense: "rgb(var(--color-primary-xIntense) / <alpha-value>)",
+          "2xIntense": "rgb(var(--color-primary-2xIntense) / <alpha-value>)",
+        },
+        secondary: {
+          "4xSubtle": "rgb(var(--color-secondary-4xSubtle) / <alpha-value>)",
+          "3xSubtle": "rgb(var(--color-secondary-3xSubtle) / <alpha-value>)",
+          "2xSubtle": "rgb(var(--color-secondary-2xSubtle) / <alpha-value>)",
+          xSubtle: "rgb(var(--color-secondary-xSubtle) / <alpha-value>)",
+          subtle: "rgb(var(--color-secondary-subtle) / <alpha-value>)",
+          moderate: "rgb(var(--color-secondary-moderate) / <alpha-value>)",
+          intense: "rgb(var(--color-secondary-intense) / <alpha-value>)",
+          xIntense: "rgb(var(--color-secondary-xIntense) / <alpha-value>)",
+          "2xIntense": "rgb(var(--color-secondary-2xIntense) / <alpha-value>)",
+        },
+        tertiary: {
+          "2xSubtle": "rgb(var(--color-tertiary-2xSubtle) / <alpha-value>)",
+          xSubtle: "rgb(var(--color-tertiary-xSubtle) / <alpha-value>)",
+          subtle: "rgb(var(--color-tertiary-subtle) / <alpha-value>)",
+          moderate: "rgb(var(--color-tertiary-moderate) / <alpha-value>)",
+          intense: "rgb(var(--color-tertiary-intense) / <alpha-value>)",
+          xIntense: "rgb(var(--color-tertiary-xIntense) / <alpha-value>)",
+          "2xIntense": "rgb(var(--color-tertiary-2xIntense) / <alpha-value>)",
+        },
+        quaternary: {
+          coolxSubtle:
+            "rgb(var(--color-quaternary-cool-xSubtle) / <alpha-value>)",
+          coolModerate:
+            "rgb(var(--color-quaternary-cool-moderate) / <alpha-value>)",
+          coolxIntense:
+            "rgb(var(--color-quaternary-cool-xIntense) / <alpha-value>)",
+        },
+        accent: {
+          "2xSubtle": "rgb(var(--color-accent-2xSubtle) / <alpha-value>)",
+          xSubtle: "rgb(var(--color-accent-xSubtle) / <alpha-value>)",
+          subtle: "rgb(var(--color-accent-subtle) / <alpha-value>)",
+          moderate: "rgb(var(--color-accent-moderate) / <alpha-value>)",
+          intense: "rgb(var(--color-accent-intense) / <alpha-value>)",
+          xIntense: "rgb(var(--color-accent-xIntense) / <alpha-value>)",
+          "2xIntense": "rgb(var(--color-accent-2xIntense) / <alpha-value>)",
+        },
+        status: {
+          failed: "rgb(var(--color-tertiary-intense) / <alpha-value>)",
+          running: "rgb(var(--color-secondary-intense) / <alpha-value>)",
+          runningSubtle: "rgb(var(--color-secondary-xSubtle) / <alpha-value>)",
+          queued: "rgb(var(--color-quaternary-cool-xIntense) / <alpha-value>)",
+          queuedSubtle:
+            "rgb(var(--color-quaternary-cool-xSubtle) / <alpha-value>)",
+          completed: "rgb(var(--color-primary-intense) / <alpha-value>)",
+          cancelled: "rgb(var(--color-foreground-cancelled) / <alpha-value>)",
+        },
+      },
+      borderColor: {
+        subtle: "rgb(var(--color-border-subtle) / <alpha-value>)",
+        muted: "rgb(var(--color-border-muted) / <alpha-value>)",
+        contrast: "rgb(var(--color-border-contrast) / <alpha-value>)",
+        disabled: "rgb(var(--color-border-disabled) / <alpha-value>)",
+        success: "rgb(var(--color-border-success) / <alpha-value>)",
+        error: "rgb(var(--color-border-error) / <alpha-value>)",
+        warning: "rgb(var(--color-border-warning) / <alpha-value>)",
+        info: "rgb(var(--color-border-info) / <alpha-value>)",
+      },
+      backgroundColor: {
+        canvasBase: "rgb(var(--color-background-canvas-base) / <alpha-value>)",
+        canvasSubtle:
+          "rgb(var(--color-background-canvas-subtle) / <alpha-value>)",
+        canvasMuted:
+          "rgb(var(--color-background-canvas-muted) / <alpha-value>)",
+        surfaceBase:
+          "rgb(var(--color-background-surface-base) / <alpha-value>)",
+        surfaceSubtle:
+          "rgb(var(--color-background-surface-subtle) / <alpha-value>)",
+        surfaceMuted:
+          "rgb(var(--color-background-surface-muted) / <alpha-value>)",
+        disabled: "rgb(var(--color-background-disabled) / <alpha-value>)",
+        contrast: "rgb(var(--color-background-contrast) / <alpha-value>)",
+        success: "rgb(var(--color-background-success) / <alpha-value>)",
+        successContrast:
+          "rgb(var(--color-background-successContrast) / <alpha-value>)",
+        error: "rgb(var(--color-background-error) / <alpha-value>)",
+        errorContrast:
+          "rgb(var(--color-background-errorContrast) / <alpha-value>)",
+        warning: "rgb(var(--color-background-warning) / <alpha-value>)",
+        warningContrast:
+          "rgb(var(--color-background-warningContrast) / <alpha-value>)",
+        info: "rgb(var(--color-background-info) / <alpha-value>)",
+        infoContrast:
+          "rgb(var(--color-background-infoContrast) / <alpha-value>)",
+        codeEditor: "rgb(var(--color-background-codeEditor) / <alpha-value>)",
+        btnPrimary: "rgb(var(--color-background-btn-primary) / <alpha-value>)",
+        btnPrimaryHover:
+          "rgb(var(--color-background-btn-primaryHover) / <alpha-value>)",
+        btnPrimaryPressed:
+          "rgb(var(--color-background-btn-primaryPressed) / <alpha-value>)",
+        btnPrimaryDisabled:
+          "rgb(var(--color-background-btn-primaryDisabled) / <alpha-value>)",
+        btnDanger: "rgb(var(--color-background-btn-danger) / <alpha-value>)",
+        btnDangerHover:
+          "rgb(var(--color-background-btn-dangerHover) / <alpha-value>)",
+        btnDangerPressed:
+          "rgb(var(--color-background-btn-dangerPressed) / <alpha-value>)",
+        btnDangerDisabled:
+          "rgb(var(--color-background-btn-dangerDisabled) / <alpha-value>)",
+      },
+      textColor: {
+        basis: "rgb(var(--color-foreground-base) / <alpha-value>)",
+        subtle: "rgb(var(--color-foreground-subtle) / <alpha-value>)",
+        muted: "rgb(var(--color-foreground-muted) / <alpha-value>)",
+        onContrast: "rgb(var(--color-foreground-onContrast) / <alpha-value>)",
+        alwaysWhite: "rgb(var(--color-foreground-alwaysWhite) / <alpha-value>)",
+        alwaysBlack: "rgb(var(--color-foreground-alwaysBlack) / <alpha-value>)",
+        disabled: "rgb(var(--color-foreground-disabled) / <alpha-value>)",
+        link: "rgb(var(--color-foreground-link) / <alpha-value>)",
+        success: "rgb(var(--color-foreground-success) / <alpha-value>)",
+        error: "rgb(var(--color-foreground-error) / <alpha-value>)",
+        warning: "rgb(var(--color-foreground-warning) / <alpha-value>)",
+        info: "rgb(var(--color-foreground-info) / <alpha-value>)",
+        btnPrimary: "rgb(var(--color-foreground-btn-primary) / <alpha-value>)",
+        btnPrimaryDisabled:
+          "rgb(var(--color-foreground-btn-primaryDisabled) / <alpha-value>)",
+        btnDanger: "rgb(var(--color-foreground-btn-danger) / <alpha-value>)",
+        btnDangerDisabled:
+          "rgb(var(--color-foreground-btn-dangerDisabled) / <alpha-value>)",
+        codeDelimiterBracketJson:
+          "rgb(var(--color-foreground-code-delimiterBracketJson) / <alpha-value>)",
+        codeStringKeyJson:
+          "rgb(var(--color-foreground-code-stringKeyJson) / <alpha-value>)",
+        codeNumberJson:
+          "rgb(var(--color-foreground-code-numberJson) / <alpha-value>)",
+        codeStringValueJson:
+          "rgb(var(--color-foreground-code-stringValueJson) / <alpha-value>)",
+        codeKeywordJson:
+          "rgb(var(--color-foreground-code-keywordJson) / <alpha-value>)",
+        codeComment:
+          "rgb(var(--color-foreground-code-comment) / <alpha-value>)",
+        codeString: "rgb(var(--color-foreground-code-string) / <alpha-value>)",
+        codeKeyword:
+          "rgb(var(--color-foreground-code-keyword) / <alpha-value>)",
+        codeEntityNameFunction:
+          "rgb(var(--color-foreground-code-entityNameFunction) / <alpha-value>)",
+      },
+      textDecorationColor: {
+        link: 'rgb(var(--color-foreground-link) / <alpha-value>)',
       },
       fontSize: {
         "5xl": ["3rem", "1.3"],
@@ -100,7 +286,10 @@ module.exports = {
     fontFamily: {
       sans: 'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
       heading: `Inter -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
-      mono: 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+      mono: 'CircularXXMono, ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+      // We can use these temporary classes while we transition to the new fonts
+      circular:
+        'CircularXX, "Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto',
     },
   },
   plugins: [require("@tailwindcss/typography")],
