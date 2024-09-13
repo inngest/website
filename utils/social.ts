bb7c9612d5ab647223ff8a2e0bc060649dbf8fe6
Dan Farrelly <djfarrelly@users.noreply.github.com>
2024-09-13 17:13:34 -0400

    Fix launch week og image (#912)
    * Fix og image url
    
    * Fix og images and metadata on app router pages

--- utils/social.ts
+++ utils/social.ts
@@ -1,3 +1,5 @@
+import { type Metadata } from "next";
+
 // Use the image version to bust social network's caches
 const openGraphImageVersion = 3;
 
@@ -9,3 +11,45 @@ export const getOpenGraphImageURL = ({ title }: { title: string }) =>
   `https://www.inngest.com/api/og?title=${encodeURIComponent(
     title
   )}&v=${openGraphImageVersion}`;
+
+const getFullURL = (absolutePath: string) =>
+  new URL(absolutePath, process.env.NEXT_PUBLIC_HOST).toString();
+
+// ...
+export const generateMetadata = ({
+  title,
+  description,
+  image,
+}: {
+  // Title without the "Inngest - " prefix
+  title: string;
+  description?: string;
+  // A relative path URL to the image
+  image?: string;
+}): Metadata => {
+  const imageUrl = image
+    ? image.match(/^\//)
+      ? getFullURL(image)
+      : image
+    : getOpenGraphImageURL({ title });
+  const metaTitle = `Inngest - ${title}`;
+  const metadata: Metadata = {
+    title: metaTitle,
+    openGraph: {
+      title: metaTitle,
+      images: [imageUrl],
+    },
+    twitter: {
+      card: "summary_large_image",
+      site: "@inngest",
+      title: metaTitle,
+      images: [imageUrl],
+    },
+  };
+  if (description) {
+    metadata.description = description;
+    metadata.openGraph.description = description;
+    metadata.twitter.description = description;
+  }
+  return metadata;
+};
