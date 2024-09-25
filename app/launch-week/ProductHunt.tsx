c904820dbc978dbf4a5bd47966c80fad25ec7013
Dan Farrelly <djfarrelly@users.noreply.github.com>
2024-09-25 09:34:17 -0400

    Bust banner cache every 60s (#940)

--- app/launch-week/ProductHunt.tsx
+++ app/launch-week/ProductHunt.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { useMemo } from "react";
 
 const url =
   "https://www.producthunt.com/posts/workflow-kit-by-inngest?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-workflow&#0045;kit&#0045;by&#0045;inngest";
 
 export default function ProductHunt() {
   // We do this b/c this is a descendent of a card which is a link. This is a hack and I'm not proud of it.
+  // cache bust every 60s
+  const ts = useMemo(() => Math.floor(Date.now() / 1000 / 60), []);
   return (
     <div
       className="cursor-pointer"
@@ -14,7 +17,7 @@ export default function ProductHunt() {
       }}
     >
       <img
-        src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=490802&theme=dark"
+        src={`https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=490802&theme=dark&ts=${ts}`}
         alt="Workflow&#0032;Kit&#0032;by&#0032;Inngest - Open&#0032;source&#0032;SDK&#0032;to&#0032;add&#0032;Zapier&#0045;like&#0032;workflows&#0032;to&#0032;your&#0032;product | Product Hunt"
         style={{ width: "250px", height: "54px" }}
         width="250"
