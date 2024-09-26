cdda367d2b5ff1c34b213869a86b840af7c54343
Charly POLY <charly@inngest.com>
2024-09-26 09:51:45 +0200

    feat(homepage): remove PH banner

--- components/LaunchWeekBanner.tsx
+++ components/LaunchWeekBanner.tsx
@@ -1,27 +1,7 @@
-"use client";
 import AnnouncementBanner from "src/components/AnnouncementBanner";
-import ProductHuntAnnouncementBanner from "src/components/ProductHuntAnnouncementBanner";
-
-function useIsItDay3InSFNow() {
-  const d = new Date();
-  const localTime = d.getTime();
-  const localOffset = d.getTimezoneOffset() * 60000;
-  const utc = localTime + localOffset;
-  const offset = -7; // UTC of PST is -05.00
-  const sfDate = new Date(utc + 3600000 * offset);
-
-  return (
-    sfDate.getDate() == 25 &&
-    sfDate.getMonth() == 8 &&
-    sfDate.getFullYear() === 2024
-  );
-}
 
 export default function LaunchWeekBanner() {
-  const day3 = useIsItDay3InSFNow();
-  return day3 ? (
-    <ProductHuntAnnouncementBanner />
-  ) : (
+  return (
     <AnnouncementBanner href="/launch-week" className="mb-4">
       Inngest Launch Week kicks off September 23rd. Follow along with all of our
       updates!
