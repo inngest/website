15e8cd4923d10b7b1acbe332c4d26398c4f07224
Dan Farrelly <daniel.j.farrelly@gmail.com>
2024-10-02 15:01:53 -0400

    Update launch week banner

--- components/LaunchWeekBanner.tsx
+++ components/LaunchWeekBanner.tsx
@@ -3,8 +3,7 @@ import AnnouncementBanner from "src/components/AnnouncementBanner";
 export default function LaunchWeekBanner() {
   return (
     <AnnouncementBanner href="/launch-week" className="mb-4">
-      Inngest Launch Week kicks off September 23rd. Follow along with all of our
-      updates!
+      Inngest Launch Week is a wrap. Check out all of the announcements!
     </AnnouncementBanner>
   );
 }
