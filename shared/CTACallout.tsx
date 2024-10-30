00c8c06ad454691385209c3959124abfcb5b00d7
Dan Farrelly <daniel.j.farrelly@gmail.com>
2024-10-30 16:13:32 -0400

    Blog CTA refresh

--- shared/CTACallout.tsx
+++ shared/CTACallout.tsx
@@ -18,14 +18,21 @@ export default function CTACallout({
     <aside
       className={`not-prose ${
         wide ? "max-w-[80ch]" : "max-w-[70ch]"
-      } m-auto bg-indigo-900/20 text-indigo-100 flex flex-col items-start gap-4 leading-relaxed rounded-lg py-5 px-6  my-12 border border-indigo-900/50`}
+      } m-auto my-12 p-px text-basis rounded-lg bg-gradient-to-br from-[rgba(var(--color-carbon-400)/0.4)] via-transparent to-transparent`}
     >
-      <p className="text-sm lg:text-base">{text}</p>
-      {!!cta && (
-        <Button href={cta.href} arrow="right">
-          {cta.text}
-        </Button>
-      )}
+      <div
+        className="flex flex-col items-start gap-5 py-5 px-6 leading-relaxed rounded-lg"
+        style={{
+          background: `linear-gradient(108deg, rgba(204, 204, 204, 0.12) 9.67%, rgba(0, 0, 0, 0.00) 49.19%), #050505`,
+        }}
+      >
+        <p className="text-sm lg:text-base">{text}</p>
+        {!!cta && (
+          <Button href={cta.href} arrow="right">
+            {cta.text}
+          </Button>
+        )}
+      </div>
     </aside>
   );
 }
