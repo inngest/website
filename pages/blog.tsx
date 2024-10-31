1f1e24061101be335d95cb0b3fb616c694aa5419
Dan Farrelly <djfarrelly@users.noreply.github.com>
2024-10-31 08:57:53 -0400

    Blog index changes. Fix  blog post image ratio. (#975)
    * Update star count
    
    * Update image. Updated blog index

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -5,6 +5,7 @@ import { Rss } from "react-feather";
 
 import { RiCalendarLine } from "@remixicon/react";
 import ArrowRight from "src/shared/Icons/ArrowRight";
+import { RiArrowRightLine } from "@remixicon/react";
 import Footer from "../shared/Footer";
 import Header from "../shared/Header";
 import Container from "../shared/layout/Container";
@@ -40,7 +41,7 @@ export default function BlogLayout(props) {
         <meta name="description" content={description}></meta>
         <meta
           property="og:title"
-          content="Inngest → Product & Engineering blog"
+          content="Inngest - Product & Engineering blog"
         />
         <meta property="og:description" content={description} />
       </Head>
@@ -66,37 +67,37 @@ export default function BlogLayout(props) {
           <div className="pt-16">
             {focus && (
               <a
-                className="relative flex flex-col-reverse lg:flex-row xl:max-w-[1160px] bg-matcha-600 rounded-lg mb-32 group shadow-lg"
+                className="relative flex flex-col-reverse lg:flex-row xl:max-w-[1160px] transition-all bg-[rgba(var(--color-foreground-success)/0.9)] hover:bg-[rgba(var(--color-foreground-success)/1)] rounded-lg mb-32 group shadow-lg"
                 href={focus.redirect ?? `/blog/${focus.slug}`}
               >
-                <div className="absolute top-0 bottom-0 -left-[40px] -right-[40px] rounded-lg bg-matcha-500 opacity-20 rotate-1 -z-0 mx-5"></div>
+                <div className="absolute top-0 bottom-0 -left-[40px] -right-[40px] rounded-lg bg-[rgba(var(--color-foreground-success)/0.2)] rotate-1 -z-0 mx-5"></div>
                 <div
                   className={`${
                     focus.image ? "lg:w-2/5" : "w-full"
                   } p-8 flex flex-col items-start justify-between relative z-10`}
                 >
-                  <div>
-                    <span className="inline-flex text-matcha-0 mb-3 text-xs font-semibold bg-matcha-700/50 px-3 py-1.5 rounded">
+                  <div className="text-alwaysBlack">
+                    <span className="inline-flex mb-3 text-xs font-semibold bg-[rgba(var(--color-carbon-50)/0.2)] px-3 py-1.5 rounded">
                       Latest Post
                     </span>
-                    <h2 className="text-xl md:text-2xl lg:text-xl xl:text-2xl text-alwaysWhite mb-1 font-medium">
+                    <h2 className="text-xl md:text-2xl lg:text-xl xl:text-2xl mb-1 font-medium">
                       {focus.heading}
                     </h2>
-                    <p className="text-basis text-sm font-medium mb-4 flex gap-1 items-center">
+                    <p className="text-sm font-medium mb-4 flex gap-1 items-center">
                       <RiCalendarLine className="h-3 w-3" />
                       {focus.humanDate} <Tags tags={focus.tags || []} />
                     </p>
-                    <p className="text-slate-100">{focus.subtitle}</p>
+                    <p className="">{focus.subtitle}</p>
                   </div>
-                  <span className="px-4 text-sm font-medium inline-flex mt-4 bg-matcha-800 text-matcha-0 py-1.5 rounded-full group-hover:bg-matcha-700">
+                  <span className="flex flex-row items-center gap-1 px-4 text-sm font-medium inline-flex mt-4 bg-carbon-900 text-carbon-50 py-1.5 rounded-lg group-hover:bg-carbon-800">
                     Read article
-                    <ArrowRight className="group-hover:translate-x-1.5 relative top-px transition-transform duration-150 " />
+                    <RiArrowRightLine className="h-4 w-4" />
                   </span>
                 </div>
                 {focus.image && (
-                  <div className="lg:w-3/5 flex rounded-t-lg lg:rounded-t-none lg:rounded-r-lg relative group-hover:scale-105 group-hover:origin-center group-hover:rounded-lg overflow-hidden transition-all lg:pr-2">
+                  <div className="lg:w-3/5 flex p-2 relative">
                     <Image
-                      className="z-10 w-full m-auto rounded-t-lg lg:rounded-lg group-hover:rounded-lg"
+                      className="z-10 w-full m-auto rounded-lg group-hover:rounded-lg"
                       src={focus.image}
                       alt={`Featured image for ${focus.heading} blog post`}
                       width={900}
