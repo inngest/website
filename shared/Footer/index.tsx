2a855cbeb3f920bb05e755c71f5253fd56a3f5b5
Dan Farrelly <daniel.j.farrelly@gmail.com>
2024-11-01 20:19:48 -0400

    Fix import case

--- shared/Footer/index.tsx
+++ shared/Footer/index.tsx
@@ -1,4 +1,4 @@
-import NextLink from "next/Link";
+import NextLink from "next/link";
 import Logo from "src/shared/Icons/Logo";
 import Discord from "../Icons/Discord";
 import Github from "../Icons/Github";
