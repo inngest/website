56c37eb617b14cbb57b306a85bc239823c6f083b
Dan Farrelly <daniel.j.farrelly@gmail.com>
2024-12-10 20:51:00 -0500

    Improve content gate

--- app/resources/access/[slug]/page.tsx
+++ app/resources/access/[slug]/page.tsx
@@ -3,41 +3,60 @@ import fs from "fs";
 import matter from "gray-matter";
 
 import { loadMarkdownFilesMetadata } from "src/utils/markdown";
-import { type BlogPost } from "src/components/Blog";
+import type { BlogPost, MDXBlogPost } from "src/components/Blog";
 import Hero from "src/components/LandingPage/Hero";
 import NewsletterSignup from "src/components/NewsletterSignup";
 
-export const metadata: Metadata = {
-  // Prevent Google from indexing gated content
-  robots: "noindex",
+type Props = {
+  params: Promise<{ slug: string }>;
 };
 
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const slug = (await params).slug;
+  const data = await loadBlogPost(slug);
+  return {
+    title: data.heading,
+    description: data.subtitle,
+    openGraph: {
+      images: [`${process.env.NEXT_PUBLIC_HOST}${data.image}`],
+    },
+    // Prevent Google from indexing gated content
+    robots: "noindex",
+  };
+}
+
 export async function generateStaticParams() {
-  const posts = await loadMarkdownFilesMetadata<BlogPost>("blog/_posts");
-  // const filteredPosts = posts.filter((p) => p?.featured !== false);
-
-  console.log("ok!", posts[0].slug);
-
+  const posts = await loadMarkdownFilesMetadata<MDXBlogPost>("blog/_posts");
   return posts.map((post) => ({
     slug: post.slug,
   }));
 }
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ slug: string }>;
-}) {
-  const slug = (await params).slug;
-
+async function loadBlogPost(slug: string) {
   let filePath = `./pages/blog/_posts/${slug}.md`;
   if (!fs.existsSync(filePath) && fs.existsSync(filePath + "x")) {
     filePath += "x";
   }
 
   const source = fs.readFileSync(filePath);
-  const { data } = matter(source);
-  const contentUrl = `/blog/${slug}`;
+  const { data, content } = matter(source);
+  const path = `/blog/${slug}`;
+
+  return {
+    ...data,
+    path,
+    content,
+  } as unknown as BlogPost & { path: string; content: string };
+}
+
+export default async function Page({ params }: Props) {
+  const slug = (await params).slug;
+  const data = await loadBlogPost(slug);
+  // Use the teaser or grab the first paragraph
+  const teaser = data.teaser ?? [
+    ...data.content.split("\n\n").slice(0, 1),
+    `Learn more about ${data.heading} in our guide today.`,
+  ];
 
   return (
     <div className="text-basis">
@@ -56,14 +75,16 @@ export default async function Page({
           id="get-access"
           className="max-w-xl mx-auto mt-4 scroll-mt-28 flex flex-col gap-6"
         >
-          {data.teaser?.map((p, idx) => (
-            <p className="text-lg md:text-xl">{p}</p>
+          {teaser?.map((p, idx) => (
+            <p className="text-lg md:text-lg" key={idx}>
+              {p}
+            </p>
           ))}
           <NewsletterSignup
             showHeader={false}
             buttonText="Get access"
             tags={[`gate-${slug}`]}
-            redirect={contentUrl}
+            redirect={data.path}
           />
         </div>
       </div>
