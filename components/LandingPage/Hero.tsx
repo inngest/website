c47b467f05507158d73c73d0338a49abe2a82c0a
Dan Farrelly <daniel.j.farrelly@gmail.com>
2025-01-09 19:33:54 -0500

    Copy updates

--- components/LandingPage/Hero.tsx
+++ components/LandingPage/Hero.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
+import Image from "next/image";
 import React from "react";
+import clsx from "clsx";
 
 export default function Hero({
   headline,
   subheadline,
   ctas = [],
+  logosHeading = "Trusted by modern software teams worldwide:",
+  logos = [],
   children,
 }: {
   headline: string;
@@ -13,6 +17,12 @@ export default function Hero({
     href: string;
     text: string | React.ReactNode;
     kind: "button" | "link";
+  }[];
+  logosHeading?: string;
+  logos?: {
+    src: string;
+    name: string;
+    scale?: number;
   }[];
   children?: React.ReactElement;
 }) {
@@ -28,7 +38,12 @@ export default function Hero({
           }}
         ></div>
       </div>
-      <div className="relative z-10 max-w-4xl mx-auto py-28 px-4 sm:px-8 text-basis text-center text-balance">
+      <div
+        className={clsx(
+          "relative z-10 max-w-4xl mx-auto py-28 px-4 sm:px-8 text-basis text-center text-balance",
+          logos?.length > 0 ? "pt-28 pb-12" : "py-28"
+        )}
+      >
         <h1 className="text-5xl leading-tight md:text-6xl md:leading-tight font-semibold bg-clip-text text-transparent bg-gradient-to-bl from-[rgb(var(--color-carbon-300))] to-[rgb(var(--color-carbon-50))]">
           {headline}
         </h1>
@@ -61,6 +76,31 @@ export default function Hero({
         </div>
         {children}
       </div>
+      {logos?.length > 0 && (
+        <div className="relative z-10 my-12 flex flex-col gap-6 items-center sm:pb-8">
+          <p className="mx-8 text-sm text-balance text-subtle">
+            {logosHeading}
+          </p>
+          <div className="grid grid-cols-2 sm:flex flex-wrap lg:flex-nowrap gap-x-4 sm:gap-x-10 gap-y-6 sm:gap-y-8 mx-8">
+            {logos.map(({ src, name, scale = 1 }, idx) => (
+              <Image
+                key={idx}
+                src={src}
+                alt={name}
+                title={name}
+                width={120 * 0.8 * scale}
+                height={30 * 0.8 * scale}
+                className={clsx(
+                  "m-auto width-auto transition-all grayscale opacity-80 invert dark:invert-0",
+                  `max-h-[${36 * scale}px]`,
+                  idx === 4 && "hidden sm:block",
+                  idx > 4 && "hidden xl:block"
+                )}
+              />
+            ))}
+          </div>
+        </div>
+      )}
       <span className="block mx-auto h-0.5 w-[84px] bg-[rgba(var(--color-foreground-base)/0.3)]"></span>
     </header>
   );
