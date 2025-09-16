47786acf6f4547ee866b91d677dcee4d3ad59013
Charly POLY <charly@inngest.com>
2025-09-16 11:14:25 +0200

    feat(website): enable banner

--- components/AnnouncementBanner.tsx
+++ components/AnnouncementBanner.tsx
@@ -21,9 +21,9 @@ const Banner: React.FC<Props> = ({ href, children, className }) => (
   <a
     href={href}
     // use the .page-banner class to hide it on select pages via CSS
-    className={`page-banner group flex items-center justify-center gap-2.5 w-full md:py-2 px-6
-                border-b border-[rgb(var(--color-primary-subtle))] pb-6 pt-3
-                text-base text-basis transition-all ${className}`}
+    className={`page-banner group flex w-full items-center justify-center gap-2.5 border-b border-[rgb(var(--color-primary-subtle))]
+                px-6 pb-6 pt-3 text-base
+                text-basis transition-all md:py-2 ${className}`}
     style={{
       backgroundImage: `
         linear-gradient(270deg, rgba(2, 117, 177, 0.80) 0%, rgba(21, 158, 136, 0.80) 0.02%, rgba(102, 189, 139, 0.80) 17.5%, rgba(45, 159, 101, 0.80) 100%)`,
@@ -32,20 +32,19 @@ const Banner: React.FC<Props> = ({ href, children, className }) => (
     <span className="rotate-45">
       <RiRocket2Fill className="h-4 w-4 group-hover:animate-[wiggle_200ms_ease-in-out_infinite]" />
     </span>
-    <span className="underline underline-offset-2 decoration-[rgba(var(--color-carbon-0)/0.5)] group-hover:decoration-[rgba(var(--color-carbon-0)/1)] transition-colors">
+    <span className="underline decoration-[rgba(var(--color-carbon-0)/0.5)] underline-offset-2 transition-colors group-hover:decoration-[rgba(var(--color-carbon-0)/1)]">
       {children}
     </span>
   </a>
 );
 
 export default function AnnouncementBanner() {
-  // Return null if we want to disable the banner
-  return null;
+  // // Return null if we want to disable the banner
+  // return null;
 
-  // return (
-  //   <Banner href="https://directus.is/inngest">
-  //     Join our "Building Advanced Content Workflows" webinar with Directus on
-  //     March 20th
-  //   </Banner>
-  // );
+  return (
+    <Banner href="/blog/announcing-inngest-series-a">
+      We raised a $21M Series A to help companies ship and iterate faster.
+    </Banner>
+  );
 }
