a95288d3c7102aa21a552e02e6bd08c390492982
Dan Farrelly <daniel.j.farrelly@gmail.com>
2026-03-23 12:04:11 -0400

    Chnge to app router

--- app/api/feedback/send/route.ts
+++ app/api/feedback/send/route.ts
@@ -1,3 +1,4 @@
+import { NextResponse } from "next/server";
 import { Inngest } from "inngest";
 
 const inngest = new Inngest({
@@ -5,32 +6,28 @@ const inngest = new Inngest({
   eventKey: process.env.NEXT_PUBLIC_INNGEST_KEY,
 });
 
-export default async (req, res) => {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
-
+export async function POST(req: Request) {
   const {
     page,
     rating,
   }: {
     page: string | null;
     rating: string | null;
-  } = req.body;
+  } = await req.json();
 
   if (!page) {
-    return res.status(400).json({ error: "Page ID is required" });
+    return NextResponse.json({ error: "Page ID is required" }, { status: 400 });
   }
 
   if (!rating) {
-    return res.status(400).json({ error: "Rating is required" });
+    return NextResponse.json({ error: "Rating is required" }, { status: 400 });
   }
 
   //  disable in development
   if (process.env.NODE_ENV === "development") {
     console.log("Skipping feedback in development");
     console.log({ page, rating });
-    return res.status(201).json({ error: "" });
+    return NextResponse.json({ error: "" }, { status: 201 });
   }
 
   try {
@@ -42,8 +39,11 @@ export default async (req, res) => {
       },
     });
 
-    return res.status(201).json({ error: "" });
+    return NextResponse.json({ error: "" }, { status: 201 });
   } catch (error) {
-    return res.status(500).json({ error: error.message || error.toString() });
+    return NextResponse.json(
+      { error: error.message || error.toString() },
+      { status: 500 }
+    );
   }
-};
+}
