4fc74e5e50bb34d42f72233dce6f76e39f98d432
Dan Farrelly <daniel.j.farrelly@gmail.com>
2026-06-04 13:14:10 -0400

    Remove unused Inter usage

--- pages/press.tsx
+++ pages/press.tsx
@@ -1,37 +1,16 @@
 import React from "react";
 import styles from "../styles/Press.module.css";
 
-const ASSETS = [
-  {
-    name: "Logo (svg)",
-    src: "/logo.svg",
-  },
-  {
-    name: "Logo (png)",
-    src: "/logo.png",
-  },
-  {
-    name: "Icon - dark (png)",
-    src: "/icon-dark.png",
-  },
-  {
-    name: "Icon - light (png)",
-    src: "/icon-light.png",
-  },
-  {
-    name: "Icon - transparent (png)",
-    src: "/icon-light-transparent.png",
-  },
-];
+const ASSETS = [];
 
 const Press = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <h1>Inngest Press Kit</h1>
       <h4>When referencing us on the web, feel free to use these assets.</h4>
-      <div className={styles.assets}>
+      <div>
         {ASSETS.map((asset) => (
-          <div className={styles.asset}>
+          <div>
             <a href={asset.src} download>
               <img src={asset.src} />
             </a>
